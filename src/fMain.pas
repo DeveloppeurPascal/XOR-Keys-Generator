ae634671f81aef7a840f0eb92dc3de33f3da2b02
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 07:28:27 +0200

    enabled replacing main form caption by about box component (#13)

--- src/fMain.pas
+++ src/fMain.pas
@@ -94,7 +94,6 @@ type
     FFreezePAramsSetValue: boolean;
     FKeySize: integer;
   protected
-    procedure InitMainFormCaption;
     procedure InitAboutDialogBox;
     procedure FillPascalCode(const Key: TList<byte>);
     procedure mmoSelectAllOnEnter(Sender: TObject);
@@ -223,7 +222,6 @@ procedure TfrmMain.FormCreate(Sender: TObject);
 var
   i: integer;
 begin
-  InitMainFormCaption;
   InitAboutDialogBox;
 
   FKeySize := 0;
@@ -282,17 +280,6 @@ begin
     'If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.'
     + slinebreak + slinebreak +
     'To find out more, visit https://xorkeysgenerator.olfsoftware.fr';
-end;
-
-procedure TfrmMain.InitMainFormCaption;
-begin
-{$IFDEF DEBUG}
-  caption := '[DEBUG] ';
-{$ELSE}
-  caption := '';
-{$ENDIF}
-  caption := caption + OlfAboutDialog1.Titre + ' v' +
-    OlfAboutDialog1.VersionNumero;
 end;
 
 procedure TfrmMain.mmoSelectAllOnEnter(Sender: TObject);
